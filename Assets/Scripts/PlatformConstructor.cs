a133411980ff87287cf54b3ef71692e96984748b
HexTree <lamencel@gmail.com>
2018-03-24 02:57:54 +0000

    platform destruction, and randomise gaps

---
+++ Assets/Scripts/PlatformConstructor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformConstructor : MonoBehaviour 
+{
+	public GameObject platform;
+	private Transform constructionPoint;
+	private float distanceBetweenMin = 1;
+	private float distanceBetweenMax = 3;
+	private float platformWidth;
+
+	// Use this for initialization
+	void Start () 
+	{
+		constructionPoint = GameObject.Find ("PlatformConstructionPoint").transform;
+		platformWidth = platform.GetComponent<BoxCollider2D> ().size.x;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (transform.position.x < constructionPoint.position.x)
+		{
+			transform.position = new Vector3(transform.position.x + platformWidth + Random.Range (distanceBetweenMin, distanceBetweenMax), 
+				transform.position.y, transform.position.z);
+				Instantiate(platform, transform.position, transform.rotation);
+		}		
+	}
+}
---
+++ Assets/Scripts/PlatformDestructor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformDestructor : MonoBehaviour {
+	private Transform destructionPoint;
+
+	// Use this for initialization
+	void Start () {
+		destructionPoint = GameObject.Find ("PlatformDestructionPoint").transform;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x < destructionPoint.position.x)
+			Destroy (gameObject);
+	}
+}
